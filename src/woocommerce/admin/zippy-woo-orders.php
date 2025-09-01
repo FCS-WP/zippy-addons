513a6c28b397b9640ad2fb0e52eedef4ca2fb858
shin <nhathanh024@gmail.com>
2025-09-01 21:46:36 +0700

    Update client feedback

--- src/woocommerce/admin/zippy-woo-orders.php
+++ src/woocommerce/admin/zippy-woo-orders.php
@@ -37,10 +37,13 @@ class Zippy_Woo_Orders
     add_action('woocommerce_order_list_table_restrict_manage_orders', array($this, 'show_filter_by_billing_date'), 1);
 
     add_filter('manage_woocommerce_page_wc-orders_columns', array($this, 'add_billing_date_col'));
+    add_filter('manage_woocommerce_page_wc-orders_columns', array($this, 'add_billing_time_col'));
 
     add_filter('woocommerce_order_query_args', array($this, 'add_meta_billing_date_query'));
+    add_filter('woocommerce_order_query_args', array($this, 'add_meta_billing_time_query'));
 
     add_action('manage_woocommerce_page_wc-orders_custom_column', array($this, 'billing_date_order_items_column'), 25, 2);
+    add_action('manage_woocommerce_page_wc-orders_custom_column', array($this, 'billing_time_order_items_column'), 25, 2);
 
     //add customer parameter for API Report
     add_filter('woocommerce_order_data_store_cpt_get_orders_query', array($this, 'handle_fulfilment_query_var'), 10, 2);
@@ -55,6 +58,84 @@ class Zippy_Woo_Orders
     echo '<div style="display:inline-flex" id="zippy_order_filter" data-name=' . BILLING_DATE . ' data-value=' . $date . '></div>';
   }
 
+
+  public function add_billing_time_col($columns)
+  {
+    // Current sort
+    $current_orderby = isset($_GET['orderby']) ? sanitize_text_field($_GET['orderby']) : '';
+    $current_order   = isset($_GET['order']) ? strtolower($_GET['order']) : 'desc';
+
+    // Default sort direction when clicking
+    $order = 'asc';
+    $class = '';
+
+    if ($current_orderby === BILLING_TIME) {
+      if ($current_order === 'asc') {
+        $order = 'desc';
+        $class = 'sorted asc';
+      } else {
+        $order = 'asc';
+        $class = 'sorted desc';
+      }
+    } else {
+      $class = 'sortable desc';
+    }
+
+    $url = add_query_arg([
+      'orderby' => BILLING_TIME,
+      'order'   => $order,
+    ]);
+
+    // Sorting indicators
+    $sort_label = '<span class="sorting-indicators ' . $current_order . '">
+        <span class="sorting-indicator asc" aria-hidden="true"></span>
+        <span class="sorting-indicator desc" aria-hidden="true"></span>
+    </span>';
+
+    $before  = array_slice($columns, 0, array_search('order_date', array_keys($columns)) + 1, true);
+    $after   = array_slice($columns, array_search('order_date', array_keys($columns)) + 1, null, true);
+
+
+
+    $new[BILLING_TIME] = sprintf(
+      '<a href="%s" class="%s"><span class="billing_date">%s</span>%s</a>',
+      esc_url($url),
+      esc_attr($class),
+      __('Fulfilment Time', 'woocommerce'),
+      $sort_label
+    );
+
+    $columns = $before + $new + $after;
+
+    return $columns;
+  }
+
+  public function add_meta_billing_time_query($args)
+  {
+    if (isset($_GET['orderby']) && $_GET['orderby'] === BILLING_TIME) {
+      $args['meta_key']  = BILLING_TIME;
+      $args['orderby']   = 'meta_value';
+
+      $args['meta_type'] = 'TEXT';
+      $args['order']     = isset($_GET['order']) ? sanitize_text_field($_GET['order']) : 'DESC';
+    }
+    if (isset($_GET[BILLING_TIME])) {
+      $date = $_GET[BILLING_TIME];
+      $args['meta_value']   = sanitize_text_field($date);
+    }
+    return $args;
+  }
+
+  public function billing_time_order_items_column($column_name, $order_or_order_id)
+  {
+
+    $order = $order_or_order_id instanceof WC_Order ? $order_or_order_id : wc_get_order($order_or_order_id);
+
+    if (BILLING_TIME === $column_name) {
+      $fulfilment_time = $order->get_meta(BILLING_TIME) ?? '';
+      echo  '<span title="converted">'  . $fulfilment_time . '</span>';
+    }
+  }
 
   public function add_billing_date_col($columns)
   {
@@ -93,6 +174,7 @@ class Zippy_Woo_Orders
     $after   = array_slice($columns, array_search('order_date', array_keys($columns)) + 1, null, true);
 
 
+
     $new[BILLING_DATE] = sprintf(
       '<a href="%s" class="%s"><span class="billing_date">%s</span>%s</a>',
       esc_url($url),
@@ -100,6 +182,7 @@ class Zippy_Woo_Orders
       __('Fulfilment Date', 'woocommerce'),
       $sort_label
     );
+
     $columns = $before + $new + $after;
 
     return $columns;
@@ -114,8 +197,8 @@ class Zippy_Woo_Orders
       $args['meta_type'] = 'DATE';
       $args['order']     = isset($_GET['order']) ? sanitize_text_field($_GET['order']) : 'DESC';
     }
-    if (isset($_GET['_billing_date'])) {
-      $date = $this->format_date_billing($_GET['_billing_date']);
+    if (isset($_GET[BILLING_DATE])) {
+      $date = $this->format_date_billing($_GET[BILLING_DATE]);
       $args['meta_value']   = sanitize_text_field($date);
     }
     return $args;
@@ -151,6 +234,13 @@ class Zippy_Woo_Orders
       $query['meta_query'][] = array(
         'key' => BILLING_DATE,
         'value' => esc_attr($query_vars[BILLING_DATE]),
+        'compare' => '='
+      );
+    }
+    if (! empty($query_vars[BILLING_TIME])) {
+      $query['meta_query'][] = array(
+        'key' => BILLING_TIME,
+        'value' => esc_attr($query_vars[BILLING_TIME]),
         'compare' => '='
       );
     }
