5bb4ebc104ecf5a7eeb41e5a5d2501ffff380a90
shin <nhathanh024@gmail.com>
2026-01-09 16:11:24 +0700

    Update the order note

--- src/woocommerce/templates/email-blocks/email-header.php
+++ src/woocommerce/templates/email-blocks/email-header.php
@@ -2,5 +2,14 @@
 
 	<p style="text-align: left;"><b>Name:</b> <?php echo $order->get_billing_first_name() . ' ' . $order->get_billing_last_name(); ?></p>
 	<p style="text-align: left;"><b>Contact:</b> <?php echo $order->get_billing_phone(); ?></p>
-	<p style="text-align: left;"><b>Postal:</b> <?php echo $order->get_billing_postcode(); ?></p>
+	<?php
+	if (!empty($order->get_billing_postcode())) {
+		echo '<p style="text-align: left;"><b>Postal Code:</b> ' . $order->get_billing_postcode() . '</p>';
+	}
+	?>
 	<p style="text-align: left;"><b>Order Status:</b> <?php echo wc_get_order_status_name($order->get_status()); ?></p>
+
+	<!-- Order Note here  -->
+	<?php if (!empty($order->get_customer_note())) {
+		echo '<p style="text-align: left;"><b>Order Note:</b> ' . esc_html($order->get_customer_note()) . '</p>';
+	} ?>
